938474dcb7f1569f86f0e92d6ea39c012cbc4c2f
Yorn <yornhamann@gmail.com>
2024-07-03 16:21:58 +0200

    Added Assets and Improved Animations

--- Assets/Scripts/Misc/CapeAnimationScript.cs
+++ Assets/Scripts/Misc/CapeAnimationScript.cs
@@ -8,11 +8,13 @@ public class CapeAnimationScript : MonoBehaviour
     [SerializeField] private Rigidbody2D rig;
     [SerializeField] private GroundedScript groundChecker;
     [SerializeField] private GameObject cape;
+    [SerializeField] private float rayCastDistance;
     private SpriteRendererScript spriteRendererScript;
     private Animator animator;
+    private bool isGrounded;
 
     private Vector3 startingPos;
-    
+    [SerializeField] private LayerMask groundMask;
 
     private void Start()
     {
@@ -25,14 +27,29 @@ public class CapeAnimationScript : MonoBehaviour
     {
         animator.SetFloat("XSpeed", Mathf.Abs(rig.velocity.x));
         animator.SetFloat("YSpeed", rig.velocity.y);
-        animator.SetBool("IsGrounded", groundChecker.isGrounded);
+        animator.SetBool("IsGrounded", isGrounded);
         
         CapePosition();
     }
 
+    private void FixedUpdate()
+    {
+        RaycastHit2D leftGroundHit = Physics2D.Raycast(rig.transform.position + new Vector3(-0.25f, 0, 0), -Vector2.up, rayCastDistance, groundMask);
+        RaycastHit2D rightGroundHit = Physics2D.Raycast(rig.transform.position + new Vector3(0.25f, 0, 0), -Vector2.up, rayCastDistance, groundMask);
+
+        if (leftGroundHit.collider || rightGroundHit.collider)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
     private void CapePosition()
     {
-        if (rig.velocity.y <= -0.1 && !groundChecker.isGrounded)
+        if (rig.velocity.y <= -0.1 && !isGrounded)
         {
             cape.transform.localPosition = new Vector3(-0.29f, 0.12f, 0f);
 
@@ -47,10 +64,16 @@ public class CapeAnimationScript : MonoBehaviour
             cape.transform.localPosition = new Vector3(-0.02f, 0f, 0f);
         }
 
-        if (rig.velocity.y >= 0.1 && rig.velocity.x >= 0.1 && !groundChecker.isGrounded )
+        if (rig.velocity.y >= 0.1 && rig.velocity.x >= 0.1 && !isGrounded )
         {
             cape.transform.localPosition = new Vector3(-0.29f, 0.12f, 0f);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(rig.transform.position + new Vector3(-0.25f, 0, 0), rig.transform.position + new Vector3(-0.25f, -rayCastDistance, 0));
         
     }
 }
